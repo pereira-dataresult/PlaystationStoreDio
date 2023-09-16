1e503963febc917fb5e83b5d8c3caa8b00518247
Yagoo Pereira <yagoosjn@gmail.com>
2023-09-16 09:45:24 -0300

    final do projeto

--- store/src/app/components/card/card.component.ts
+++ store/src/app/components/card/card.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent {
+export class CardComponent implements
+
+	OnInit {
+
+		@Input()
+		gameCover:string = ""
+		@Input()
+		gameType:string = ""
+		@Input()
+		gamePrice:string = ""
+		@Input()
+		gameLabel : string = ""
+
+		constructor() {}
+
+		ngOnInit(): void {
+
+		}
+
 
 }
